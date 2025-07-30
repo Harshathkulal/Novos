77e9656335308d9ec2b4d39781da84f952239436
harshath <harshathmkulal@gmail.com>
2025-07-30 21:45:09 +0530

    feat: add helmet middleware for enhanced security

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,10 +4,12 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user-route";
 import messageRoutes from "./routes/message-route";
+import helmet from "helmet";
 
 const app = express();
 
 // Middleware
+app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 app.use(
