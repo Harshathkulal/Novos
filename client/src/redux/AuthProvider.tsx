0bef145e99c8d0dba37e8bb4f36d3c52c423d260
harshath <harshathmkulal@gmail.com>
2025-08-09 00:30:00 +0530

    minor change

--- client/src/redux/AuthProvider.tsx
+++ client/src/redux/AuthProvider.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import { createContext, useContext, useEffect, useState } from "react";
-import { useRouter, usePathname } from "next/navigation";
+import { createContext, useContext, useEffect, useState, useCallback } from "react";
 import { getUser } from "@/services/userService";
 import { AuthContextType } from "@/types/main.type";
 import { Loader } from "lucide-react";
-const publicPaths = ["/login", "/signup"];
 
 const AuthContext = createContext<AuthContextType>({
   userId: null,
@@ -20,10 +18,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  const router = useRouter();
-  const pathname = usePathname();
-
-  const fetchUser = async () => {
+  const fetchUser = useCallback(async () => {
     setLoading(true);
     try {
       const res = await getUser();
@@ -35,24 +30,11 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     } finally {
       setLoading(false);
     }
-  };
-
-  useEffect(() => {
-    fetchUser();
   }, []);
 
   useEffect(() => {
-    if (!loading) {
-      const isPublic = publicPaths.includes(pathname);
-      if (!userId && !isPublic) {
-        router.replace("/login");
-      }
-
-      if (userId && isPublic) {
-        router.replace("/");
-      }
-    }
-  }, [loading, userId, pathname, router]);
+    fetchUser();
+  }, [fetchUser]);
 
   if (loading) {
     return (
@@ -64,9 +46,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   }
 
   return (
-    <AuthContext.Provider
-      value={{ userId, loading, error, refetch: fetchUser, setUserId }}
-    >
+    <AuthContext.Provider value={{ userId, loading, error, refetch: fetchUser, setUserId }}>
       {children}
     </AuthContext.Provider>
   );
